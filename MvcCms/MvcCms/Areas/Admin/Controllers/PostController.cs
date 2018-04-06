d842d69db6dea92a93eb71cca7b61c63a87f5480
subash karki <karki011@gmail.com>
2018-04-06 12:56:58 -0500

    Create control for model to post, create, edit boilerplate

---
+++ MvcCms/MvcCms/Areas/Admin/Controllers/PostController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCms.Models;
+
+namespace MvcCms.Areas.Admin.Controllers
+{
+    // /admin/post
+
+    [RouteArea("Admin")]
+    [RoutePrefix("posts")]
+    public class PostController : Controller
+    {
+        // GET: Admin/Post
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // /admin/post/create
+        [HttpGet]
+        [Route("create")]
+        public ActionResult Create()
+        {
+            var model = new Post();
+
+            return View(model);
+
+        }
+
+        // /admin/post/create
+        [HttpPost]
+        [Route("create")]
+        public ActionResult Create(Post model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            // TODO: update model in date source
+
+            return RedirectToAction("index");
+
+        }
+
+        // /admin/post/edit/post-to-edit
+        [HttpGet]
+        [Route("edit/{id}")] //what post you want to edit?
+        public ActionResult Edit(string id)
+        {
+            //TODO: to retrive the model from the data store
+            var model = new Post();
+
+            return View(model);
+        }
+
+        // /admin/post/edit/post-to-edit
+        [HttpPost]
+        [Route("edit/{id}")] //what post you want to edit?
+        public ActionResult Edit(Post model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            
+            // TODO: update model in date source
+
+            return RedirectToAction("index");
+        }
+    }
+}
